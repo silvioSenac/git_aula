a6ff617384316e5268aedccd80b9f62d1e34f4f7
Silvio Percicotte Junior <8050@tcesc.tc.br>
2022-04-15 18:41:15 -0300

    alterando a classe entidade do cliente

--- src/main/java/br/com/senac/entidade/Cliente.java
+++ src/main/java/br/com/senac/entidade/Cliente.java
@@ -6,6 +6,7 @@
 package br.com.senac.entidade;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.*;
 
 /**
@@ -15,9 +16,16 @@ import javax.persistence.*;
 @Entity
 @Table(name = "cliente")
 public class Cliente implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false)
+    private String nome;
+    
 
     public Long getId() {
         return id;
@@ -26,5 +34,37 @@ public class Cliente implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+    
     
 }
